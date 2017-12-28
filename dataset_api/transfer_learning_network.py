ff8792dc0603d5a10bf2aca4524cea4f701cdd99
Sahil Singh <sahilsingh90@gmail.com>
2017-12-28 11:55:28 +0530

    Added in-depth comments for transfer learning model

--- dataset_api/transfer_learning_network.py
+++ dataset_api/transfer_learning_network.py
@@ -2,9 +2,13 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
+# Get MNIST data without using the `one_hot = True` flag as
+# we will be using the `tf.nn.sparse_softmax_cross_entropy_with_logits` function.
 mnist = input_data.read_data_sets("MNIST_data/")
 
 x = tf.placeholder(tf.float32, shape=[ None, 784], name='inputs')
+# Use dtype as `tf.int32` specifying output type from `tf.argmax` to be `tf.int32`
+# (It defaults to `tf.int64`)
 y = tf.placeholder(tf.int32, shape=[ None ], name='labels')
 
 # Hyperparameters
@@ -36,17 +40,29 @@ test_data = tf.data.Dataset.from_tensor_slices({
     'labels': mnist.test.labels[filtered_test_ixs]}) \
             .batch(len(filtered_test_ixs))
 
+# We use the initializable iterator as we'll re-run through all the batches
+# in the training dataset, initializing the spent iterator every epoch.
 training_iterator = training_data.make_initializable_iterator()
 next_training_batch = training_iterator.get_next()
 
+# Since the validation set as a whole will be used over and over 
+# every epoch, we specified the batch_size to be the entire dataset size
+# and made it `repeat()` again and again.
 validation_iterator = validation_data.make_one_shot_iterator()
 next_validation_batch = validation_iterator.get_next()
 
+# We'll use the test dataset only once so make a one-shot iterator
+# with the batch_size as the entire dataset.
 test_iterator = test_data.make_one_shot_iterator()
 next_test_batch = test_iterator.get_next()
 #####
 
 ##### Begin Model Definition
+# Use the _restorable_ name scope as these variables should not be initialized
+# and we will use this name scope to filter them out.
+# Also, we set `trainable = False` as we are using these layers for feature extraction
+# and not to fine-tune the network. If you'd like to fine-tune the network, then set 
+# `trainable = True`.
 with tf.name_scope("restorable"):
     # Layer 1
     with tf.name_scope("layer_1"):
@@ -63,6 +79,7 @@ with tf.name_scope("restorable"):
         act_2 = tf.nn.relu(linear_2, name='activation')
 
 # Layer 3
+# We use 3 output nodes instead of 10.
 with tf.name_scope("layer_3_new"):
     w3 = tf.Variable(tf.truncated_normal(shape=(10, 3), mean=0, stddev=1), name='weights')
     b3 = tf.Variable(tf.zeros([3]), name='biases')
@@ -93,12 +110,24 @@ with tf.name_scope('accuracy'):
 
 
 with tf.Session() as sess:
+    # Filter out the graph for variables which will be restored from previously
+    # trained models and thus should not be initialized.
     init_vars = [var 
             for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
             if not var.name.startswith("restorable")]
 
+    # Using `tf.variable_initializer` and passing in the list of variables
+    # we want initialized instead of using `tf.global_variables_initializer`.
     sess.run(tf.variables_initializer(var_list = init_vars))
 
+    # Restore model weights from the previously trained network.
+    # The keys in the dictionary passed to `var_list` represent the variable
+    # name in the previously trained networks and the values represent the 
+    # variables these weights should be assigned to. 
+    # We could have used a list of names if the variables names in the current network
+    # were the same as those in the previous network but as we used the _restorable_
+    # namespace, the correspondence was broken.
+    # See: https://www.tensorflow.org/programmers_guide/saved_model
     saver = tf.train.Saver(var_list={
         "layer_1/weights":w1,
         "layer_1/biases" :b1,
@@ -126,7 +155,7 @@ with tf.Session() as sess:
 
                 _, loss = sess.run([train_opt, total_loss], feed_dict = feed_dict)
 
-            # Calculate validation accuracy every epoch.
+            # Calculate validation accuracy every epoch once the iterator has exhausted all batches.
             except tf.errors.OutOfRangeError:
                 validation_data = sess.run(next_validation_batch)
                 valid_acc = sess.run(accuracy, feed_dict = {
